9c2a5e8fb592a657e25ebf067d2a086ef25c1e88
JMieding <jamesmieding@Guest.local>
2015-10-12 14:32:24 -0500

    initialize advanced_programs dir, add word_analyzer subdir and files.

---
+++ advanced_programs/word_analyzer/word_analyzer.py
@@ -0,0 +1,38 @@
+"""
+This is a homework assignment from Google's Python course. https://developers.google.com/edu/python/?hl=en
+This program uses python 2.7.10 to sort and return all words in a user-provided text file. 
+
+This program requires importing the prettytable module to display the results.
+You can install the prettytable module with with the command: 'easy_install prettytable'
+"""
+
+import re
+from prettytable import PrettyTable
+
+display = PrettyTable(["Word", "Times in File"])
+text_file = open(raw_input("What file do you want to analyze? "))
+words_in_file = re.findall(r'\w+\'\w+|\'\w+|\w+\'|\w+\-\w+|\w+', text_file.read())
+table = {}
+
+for word in words_in_file:
+  word = word.lower()
+  if word in table:
+	table[word] += 1
+  else:
+	table[word] = 1
+
+# Sort Words by Frequency
+def count_words_frequency():
+  words_sorted = sorted(table.items(), key=lambda tup: tup[1], reverse = True)
+  for tup in words_sorted:
+    display.add_row([tup[0], tup[1]])
+  print display
+		
+# Sort Words Alphabetically
+def count_words():
+  for word in sorted(table.keys()):
+    display.add_row([word, table[word]])
+  print display
+  
+count_words_frequency()
+# count_words()
