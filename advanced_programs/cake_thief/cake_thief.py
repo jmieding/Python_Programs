fc4c7d83989ac32cbbf5569def14d18515f7b1fd
JMieding <jamesmieding@Guest.local>
2015-11-10 11:17:38 -0600

    Refactor using itertools. Itertools is AWESOME. Also, overhaul test module to address refactored code.

--- advanced_programs/cake_thief/cake_thief.py
+++ advanced_programs/cake_thief/cake_thief.py
@@ -1,151 +1,60 @@
-# Python 2.7.10
-# This program is designed to solve a programming puzzle called "Cake Thief." 
-# You can find this and other programming puzzles at Interviewcake.com.
 
-# Premise: a thief is breaking into a bakery to steal cakes. There are a finite 
-# number of cake types (cheese cake, chocolate cake, etc.), but each type has an 
-# infinite number of cakes. All stolen cakes must fit in a duffel bag. The duffel 
-# bag can hold an infinite volume, but can only hold a finite amount of weight. 
-# The challenge is to write a function that returns which cakes, if stolen, will 
-# result in the most valuable heist, given 1) a list [array] of tuples with two 
-# values, where the first value is an integer representing the $ value of the cake,
-# and the second is an integer representing the weight in lbs. of the cake; and 2) 
-# a max weight that the duffel bag can hold in pounds. Weights and values may be any 
-# non-negative integer, including zero.
+"""
+Python 2.7.10
 
+This program is designed to solve a programming puzzle called "Cake Thief." 
+You can find this and other programming puzzles at Interviewcake.com.
 
+Premise: a thief is breaking into a bakery to steal cakes. There are a finite 
+number of cake types (cheese cake, chocolate cake, etc.), but each type has an 
+infinite number of cakes. All stolen cakes must fit in a duffel bag. The duffel 
+bag can hold an infinite volume, but can only hold a finite amount of weight. 
+The challenge is to write a function that returns which cakes, if stolen, will 
+result in the most valuable heist, given 1) a list of tuples with two 
+values, where the first value is an integer representing the value in dollars($) of 
+the cake, and the second is an integer representing the weight in lbs. of the cake; 
+and 2) a max weight that the duffel bag can hold in pounds. Weights and values may 
+be any non-negative integer, including zero.
+"""
 
-# set decimal precision for cake $/lb ratios
-from decimal import *
-getcontext().prec = 4
+from itertools import combinations_with_replacement
 
-# define function to return optimal cake heist given a list of cakes and duffel capacity
 def cake_heist_calculator(raw_cakes, capacity):
-
-  remove_list = [] # holds cakes that will be removed from duffel after substitution is complete.
-  duffel = []  # holds the final heist
-  cakes_with_ratio = [] # holds raw cakes with their $/lb ratio.
-
-
-# Calculate cakes with highest $/lbs ratio and include the ratio as the third 
-# value of each tuple. Create a new list duffel to hold new tuples. Exclude 
-# from the new list cakes heavier than the max capacity of the bag, and cakes 
-# that weigh zero (these would fit in the bag an infinite number of times). Sort 
-# this list from highest $/lbs to lowest.
-
-
-  # exclude cakes that 1) will not fit into the bag, 2) weigh zero lbs., 
-  # or 3) have a value of zero or less.
-  for tup in raw_cakes:
-    if tup[1] <= capacity and tup[1] > 0 and tup[0] > 0:
-      cakes_with_ratio.append((tup[0], tup[1], float(Decimal(tup[0]) / Decimal(tup[1]))))
+# define function to return optimal cake heist given a list of cakes and duffel capacity
+  
+  eligible_cakes = [] 
+  # stores cakes that meet criteria below. See comment in "for cake in raw_cakes" loop.
+  for cake in raw_cakes:
+    if 0 < cake[1] <= capacity and cake[0] > 0: 
+    # cakes must be heavier than 0lbs., but lighter than bag capacity, and must be
+    # worth more than $0.
+      eligible_cakes.append(cake)
     else:
-      raw_cakes.pop(raw_cakes.index(tup))
-  # sort qualifying cakes from highest $/lb ratio to lowest.
-  cakes_sorted_by_ratio = sorted(cakes_with_ratio, key= lambda tup: tup[-1], reverse=True) 
-  
-  # begin adding cakes to the bag starting with the type with the highest $/lbs. 
-  # Add as many cakes of that type as possible. When no more fit, do the same 
-  # for the type with next highest ratio, and so on until the duffel cannot hold another cake.
-  for tup in cakes_sorted_by_ratio:
-    while capacity >= tup[1]:
-      duffel.append(tup)
-      capacity -= tup[1]
+      pass
 
-  # if the bag is full, or there are no cakes to attempt substituting, this is the optimal 
-  # solution. Print the cakes included in the duffel. Use a dictionary for readability.
-  if capacity == 0 or len(cakes_sorted_by_ratio) < 2:
-    return final_view(duffel)
-  
+  all_combinations = [] # stores all combinations of cakes that do not exceed bag capacity.
+  smallest_cake = min(eligible_cakes, key= lambda cake: cake[1])[1]
+  # this value is used to calculate the max number of cakes allowed in a combination
 
-# If the bag is not full, it is possible that there is a better solution. 
-# For example, suppose bag capacity is 100lbs, and there are two types of 
-# cakes: ($100, 60lbs), ($60, 50lbs). Without more code, the program will 
-# return one ($100, 60lb) cake as the optimal solution because it has the 
-# highest $/lbs ratio, leaving the thief with a heist of $100, when the optimal 
-# solution is two cakes with a slightly lower $/lbs ratio ($60, 50lbs) 
-# resulting in a heist of $120.
+  for combination_length in xrange(1, int(capacity/smallest_cake)+1):
+  # each value is a possible number of cakes in a combination
+    for combination in combinations_with_replacement(eligible_cakes, combination_length):
+    # each value is a tuple of tuples
+      if sum(weight for value, weight in combination) > capacity:
+        pass
+      # if the combination weight > capacity, ignore it.
+      else:
+        all_combinations.append(combination)
+      # collect all combinations that fit within bag capacity
 
-# To avoid this, first, evaluate whether replacing the heaviest cake in 
-# the duffel with a combination of lighter cakes increases the heist value. 
-# Of the cakes with a lighter weight, start by trying the cake with the highest 
-# $/lbs ratio. Add as many as will fit, then do the same with the remaining cakes
-# until the bag cannot hold another cake. If replacing the heavier cake with the 
-# lighter ones results in a larger heist, then remove the heavy cake from the bag, 
-# add the lighter cakes, and repeat the process with the next heaviest cake
-# (including the cakes just inserted). Stop when BOTH a substitution results in a 
-# heist value <= the heavier cake AND you have checked all possible combinations of 
-# lighter cakes for that substitution. Terminating the program after ONLY checking 
-# for a substitution that results in a lower value may not be the best solution. See 
-# test_skip_lower_value_cake in test_cake_thief.py file.
+  best_combination = max(all_combinations, key= lambda combination: sum(value for value, weight in combination))
+  # find highest value combination.
 
-  else:
-    duffel = sorted(duffel, key= lambda tup: tup[1], reverse=True)
-    capacity += sum(y for x, y, z in duffel) # reset capacity to empty duffel. See next comments.
-    for tup in duffel:
-
-      # calculate capacity of the duffel without the heaviest cake by 
-      # subtracting all other cakes from total capacity
-      capacity -= sum(y for x, y, z in duffel[(duffel.index(tup) + 1):])
-
-      cake_check(cakes_sorted_by_ratio, tup, duffel, capacity, remove_list)
-    return final_view(duffel)
-
-# converts duffel from list to dictionary
-def final_view(duffel):
-  duffel_optimized = {}
-  for tup in duffel:
-    if tup in duffel_optimized:
-      duffel_optimized[tup] += 1
+  best_combination_dict = {}
+  for cake in best_combination:
+    if cake in best_combination_dict:
+      best_combination_dict[cake] += 1
     else: 
-      duffel_optimized[tup] = 1
-  return duffel_optimized
-
-def cake_check(cakes_sorted_by_ratio, tup, duffel, capacity, remove_list):
-  temp_heist = 0 # combined value of would-be replacements for the heavier cake
-  better_cakes = [] # cakes that fit into the space left by the heavier cake
-  for cake in cakes_sorted_by_ratio:
-    # skip over a replacement cake if it is identical to the cake removed or if it won't 
-    # fit in the remaining duffel capacity.
-    if cake[1] >= tup[1] or cake[1] > capacity:
-      pass
-    # if a cake fits, add as many as possible, then try the next best cake.      
-    else:
-      while capacity >= cake[1]:
-        better_cakes.append(cake) # list that will hold possible combination of replacement cakes
-        capacity -= cake[1]
-        temp_heist += cake[0] # used to compare value of would-be replacements a the cake in the duffel
-
-  # if the replacements are a higher value than the original cake, 1) add the replaced cake 
-  # to a temporary list (remove_list) to avoid deleting an item in the duffel while iterating 
-  # over it, (After the iteration is complete, the program will remove all cakes from the duffel
-  # that appear in the remove_list) and 2) add the replacement cakes to the duffel.
-  if tup[0] < temp_heist:
-    remove_list.append(tup)
-    for cake in better_cakes:
-      duffel.append(cake)
-   
-  # if the combination of would-be replacements is not as valuable as the original cake, and there are 
-  # still possible combinations to try (i.e., better_cakes is not Null), remove the first cake from 
-  # the combination of better_cakes and attempt to substitute again.      
-  elif tup[0] > temp_heist and better_cakes: 
-    for cake in better_cakes:
-      capacity += better_cakes[0][1] # reset capacity to reflect removing the would-be replacements
-      temp_heist -= better_cakes[0][0] # reset temp_heist to reflect removing the would-be replacements
-    try:
-      cakes_sorted_by_ratio.pop(cakes_sorted_by_ratio.index(better_cakes[0]))
-    except ValueError: # if cake in substitute cake has already been removed from sorted_cakes, pass
-      pass
-    cake_check(cakes_sorted_by_ratio, tup, duffel, capacity, remove_list)
-    
-  # if there are no possible combinations that can fit into the remaining capacity (i.e. better_cakes 
-  # is Null), then all combinations have been tried and there is not better solution. If there are any 
-  # cakes that have been replaced, remove them from the duffel. Convert the duffel to dictionary for readability.
-  else:
-    if remove_list:
-      for item in remove_list:
-        if item in duffel:
-          duffel.pop(duffel.index(item))
-    else:
-      pass
-    return final_view(duffel)
+      best_combination_dict[cake] = 1
+  return best_combination_dict
+  # convert to Dictionary for readability. I don't care about cake order, so it's faster than built-in Counter() function.
--- advanced_programs/cake_thief/test_cake_thief.py
+++ advanced_programs/cake_thief/test_cake_thief.py
@@ -5,35 +5,33 @@ from cake_thief import cake_heist_calculator
 capacity = 100
 raw_cakes_1 = [(100, 60)]
 raw_cakes_2 = [(100, 60), (10, 0)]
-raw_cakes_3 = [(100, 60), (60, 50)]
-raw_cakes_4 = [(100, 60), (50, 50)]
-raw_cakes_5 = [(119, 70), (100, 60), (60, 50)]
-raw_cakes_6 = [(119, 70), (100, 60), (60, 50), (25, 40)]
-raw_cakes_7 = [(5, 100), (20, 75), (17, 45), (5, 27), (75, 40), (45, 100), (0, 10), (10, 0), (5, 19)]
+raw_cakes_3 = [(500, 101), (60, 50)]
+raw_cakes_4 = [(100, 60), (60, 50)] 
+raw_cakes_5 = [(100, 60), (50, 50)]
+raw_cakes_6 = [(500, 101), (100, 60), (60, 50), (50, 50), (10, 0)]
 
 def test_one_cake():
-    assert cake_heist_calculator(raw_cakes_1, capacity) == {(100, 60, 1.667): 1}
+    assert cake_heist_calculator(raw_cakes_1, capacity) == {(100, 60): 1}
     # no errors when only one cake in bakery
 
-def test_simple_case():
-    assert cake_heist_calculator(raw_cakes_3, capacity) == {(60, 50, 1.2): 2}
-    # program should substitute smaller cakes for larger, where doing so increases heist value
+def test_exclude_cakes_with_zero_weight():
+    assert cake_heist_calculator(raw_cakes_2, capacity) == {(100, 60): 1}
+    # If failed, would result in infinite solution.
+
+def test_exclude_cakes_heavier_than_bag_capacity():
+    assert cake_heist_calculator(raw_cakes_3, capacity) == {(60, 50): 2}
+    # self explanatory
  
-def test_zero_cake_weight():
-    assert cake_heist_calculator(raw_cakes_2, capacity) == {(100, 60, 1.667): 1}
-    # program should ignore cakes with zero weight. Would result in infinite solution.
-    
+def test_find_highest_value_combination():
+    assert cake_heist_calculator(raw_cakes_4, capacity) == {(60, 50): 2}
+    # when the value of a single cake is exceeded by the combined value of smaller cakes, 
+    # program should return the combination of smaller cakes.
+
 def test_optimize_weight():
-    assert cake_heist_calculator(raw_cakes_4, capacity) == {(100, 60, 1.667): 1}
-    # to keep duffel light-weight, program should not substitute smaller cakes with the same aggregate value as a larger cake
+    assert cake_heist_calculator(raw_cakes_5, capacity) == {(100, 60): 1}
+    # when two or more combinations are of equal value, program should return
+    # cake combination with lightest weight.
+
+def test_combine_all_tests():
+    assert cake_heist_calculator(raw_cakes_6, capacity) == {(60, 50): 2}
     
-def test_skip_lower_value_cake():
-    assert cake_heist_calculator(raw_cakes_5, capacity) == {(60, 50, 1.2): 2}
-    # program should not stop after an attempted substitution results in a lower heist.
-
-def test_include_all_possible_combinations():
-    assert cake_heist_calculator(raw_cakes_6, capacity) == {(100, 60, 1.667): 1, (25, 40, 0.625): 1}
-    # program should continue substituting until all combinations have been tried.
-
-def test_other_combinations():
-    assert cake_heist_calculator(raw_cakes_7, capacity) == {(75, 40, 1.875): 2, (5, 19, 0.2632): 1}
