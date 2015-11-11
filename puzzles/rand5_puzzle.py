1764166f84ba9ea830529df5326d407997976480
JMieding <jamesmieding@Guest.local>
2015-11-11 17:49:03 -0600

    Refactor eliminating if, elif, else statements

--- puzzles/rand5_puzzle.py
+++ puzzles/rand5_puzzle.py
@@ -7,57 +7,52 @@ integers from 1 - 7.
 
 """
 
+import timeit
 import random
-
-results = {
-	1: 0,
-	2: 0,
-	3: 0,
-	4: 0,
-	5: 0,
-	6: 0,
-	7: 0,
-}
 
 def rand5():
 	return random.randrange(1, 6)
 
 def rand7():
-# should return random integer from 1- 7
+	
 	conversion = 0
+	
 	for i in xrange(6):
 		conversion += rand5()
-	
 	conversion -= 1
+	# adjusts range of outcomes from 6 - 30 to 5 - 29. After adjustment,
+	# there are an equal number of outcomes higher and lower than the 
+	# target range, preventing the function from weighting some numbers.
 
-	# without subtraction, range of outcomes is 6 - 30, which means
-	# rand5 can produce 1 number lower and 3 numbers higher than
-	# our target range. Subtraction rebalances the range so
-	# that there are two numbers higher and lower than the 
-	# range: 5, 6 and 28, 29. Distribution of 1 - 7, will therefore
-	# be equal.
-
-	if conversion not in xrange(7, 28):
+	if conversion in xrange(7, 28):
+		conversion = (conversion % 7) + 1
+	else:
 		rand7()
-	elif conversion in [7, 14, 21]:
-		results[1] += 1
-	elif conversion in [8, 15, 22]:
-		results[2] += 1
-	elif conversion in [9, 16, 23]:
-		results[3] += 1
-	elif conversion in [10, 17, 24]:
-		results[4] += 1
-	elif conversion in [11, 18, 25]:
-		results[5] += 1
-	elif conversion in [12, 19, 26]:
-		results[6] += 1
-	elif conversion in [13, 20, 27]:
-		results[7] += 1
-	else: 
-		pass
+	# theoretically, this could result in a stack overflow, but the odds
+	# of that happening are close to zero. The probability of recursively 
+	# calling rand7() 3 times in a row is (4/30)^3, or about 0.2%. While
+	# loop is an alternative, but it didn't consistently increase speed,
+	# so I chose to leave it.
 
-# to check even distribution.
-for i in xrange(10000):
-	rand7()
+	return conversion
 
-print results
+# Test for even distribution
+def test_rand7(times):
+	
+	results = {
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+		6: 0,
+		7: 0,
+	}
+
+	for i in xrange(times):
+		results[(rand7() % 7) + 1] += 1
+
+	print results
+
+# times = 1000000
+test_rand7(times)
